53f6167166bb5f073c2aab8beebe9c3f0d335e0b
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-07-30 15:05:49 +0500

    scaffolding the app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>hello</div>
+        );
+    }
+
+}
+
+export default App;
